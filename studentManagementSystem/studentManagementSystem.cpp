600b8ce7559cc173b971bcd439ff820bad586d36
Amin <bihamtaamin81@gmail.com>
2023-06-01 18:00:56 +0800

    Seperated classes from main

--- studentManagementSystem/studentManagementSystem.cpp
+++ studentManagementSystem/studentManagementSystem.cpp
@@ -1,114 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Classes.cpp"
 using namespace std;
-int objectNum = 0;
-
-class Person {
-private:
-    string name, email, phoneNum;
-public:
-
-    // Getters
-    string getName() const { return name; }
-    string getEmail() const { return email; }
-    string getPhoneNum() const { return phoneNum; }
-
-    // Setters
-    void setName(string _name) { name = _name; }
-    void setEmail(string _email) { email = _email; }
-    void setPhoneNum(string _phoneNum) { phoneNum = _phoneNum; }
-
-    // Constructor
-    Person(string _name = "", string _email = "", string _phoneNum = "") {
-        name = _name;
-        email = _email;
-        phoneNum = _phoneNum;
-    }
-};
-
-class Student : public Person {
-private:
-    string matricNum;
-    int dateOfBirth;
-    string nationality;
-    string passNum;
-    int semester;
-    string AA;
-    float CGPA;
-
-public:
-
-    Student(string _name = "", string _email = "", string _phoneNum = "", string _matricNum = "", int _dob = 0, string _nationallity = "", string _passNum = "", int _sem = 0, string _aa = "", float _cgpa = 0) {
-        setName(_name);
-        setEmail(_email);
-        setPhoneNum(_phoneNum);
-        setMatricNum(_matricNum);
-        setDateOfBirth(_dob);
-        setNationality(_nationallity);
-        setPassNum(_passNum);
-        setSemester(_sem);
-        setAA(_aa);
-        setCGPA(_cgpa);
-    }
-
-    // Getter and Setter for matricNum
-    string getMatricNum() const {
-        return matricNum;
-    }
-    void setMatricNum(const string& matricNum) {
-        this->matricNum = matricNum;
-    }
-
-    // Getter and Setter for dateOfBirth
-    int getDateOfBirth() const {
-        return dateOfBirth;
-    }
-    void setDateOfBirth(int dateOfBirth) {
-        this->dateOfBirth = dateOfBirth;
-    }
-
-    // Getter and Setter for nationality
-    string getNationality() const {
-        return nationality;
-    }
-    void setNationality(const string& nationality) {
-        this->nationality = nationality;
-    }
-
-    // Getter and Setter for passNum
-    string getPassNum() const {
-        return passNum;
-    }
-    void setPassNum(const string& passNum) {
-        this->passNum = passNum;
-    }
-
-    // Getter and Setter for semester
-    int getSemester() const {
-        return semester;
-    }
-    void setSemester(int semester) {
-        this->semester = semester;
-    }
-
-    // Getter and Setter for AA
-    string getAA() const {
-        return AA;
-    }
-    void setAA(const string& AA) {
-        this->AA = AA;
-    }
-
-    // Getter and Setter for CGPA
-    float getCGPA() const {
-        return CGPA;
-    }
-    void setCGPA(float CGPA) {
-        this->CGPA = CGPA;
-    }
-
-};
+int objectNum = 0;  
 
 void findNumOfRecords() {
     ifstream inputFile("students.txt");
@@ -164,5 +59,7 @@ int main()
     findNumOfRecords();
     Student* students = new Student[objectNum];
     readStudent(students);
+
+    cout << students[1].getName();
 
 }
